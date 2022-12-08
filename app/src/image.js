66989dafb71c5ab0a38f5e5ac532773d4dde67fe
VitorOdorico <ve743137@gmail.com>
2022-12-08 11:38:57 -0300

    add implementation image.js

--- app/src/image.js
+++ app/src/image.js
@@ -1,19 +1,24 @@
-let imagemDoAtor;
 let imagemDaEstrada;
+let imagemDoAtor;
 let imagemCarro;
 let imagemCarro2;
 let imagemCarro3;
+let imagemCarro4;
+let imagemCarro5;
+let imagemCarro6;
+
 
 // list imagem carros
 let imagemCarros = [imagemCarro,imagemCarro2,imagemCarro3,imagemCarro4,imagemCarro5,imagemCarro6];
 
+
 function preload(){
-    imagemDaEstrada = loadImage("images/estrada.png")
-    imagemDoAtor = loadImage("images/ator-1.png")
-    imagemCarros[0] = loadImage("images/carro-1.png")
-    imagemCarros[1] = loadImage("images/carro-2.png")
-    imagemCarros[2] = loadImage("images/carro-3.png")
-    imagemCarros[3] = loadImage("images/carro-2.png")
-    imagemCarros[4] = loadImage("images/carro-1.png")
-    imagemCarros[5] = loadImage("images/carro-3.png")
-  }
+  imagemDaEstrada = loadImage("images/estrada.png")
+  imagemDoAtor = loadImage("images/ator-1.png")
+  imagemCarros[0] = loadImage("images/carro-1.png")
+  imagemCarros[1] = loadImage("images/carro-2.png")
+  imagemCarros[2] = loadImage("images/carro-3.png")
+  imagemCarros[3] = loadImage("images/carro-2.png")
+  imagemCarros[4] = loadImage("images/carro-1.png")
+  imagemCarros[5] = loadImage("images/carro-3.png")
+}
