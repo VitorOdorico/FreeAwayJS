8332ffef486c49b30257202fd3a4311aa7c9de76
VitorOdorico <ve743137@gmail.com>
2022-12-08 11:43:09 -0300

    add placar implementations

--- app/placar.js
+++ app/placar.js
@@ -1,5 +1,30 @@
-for (let i =0;i<imagemCarros.length;i++){
-    xCarros[i] -= velocidadeCarros[i]
-    xCarros[i] += velocidadeCarros[i]
-}
-
+function verificarColisao(){
+    for(let i = 0;i < imagemCarros.length; i++){
+      colisao = collideRectCircle(xCarros[i],yCarros[i],50,40,xAtor,yAtor,15)
+      if(colisao){
+         // yAtor = 500
+        xAtor = 243;
+        yAtor = 366;
+      }
+    }
+  }
+  
+  
+  let meusPontos = 0;
+  function placar(){
+    textSize(30)
+    fill(color(255,255,0))
+    text(meusPontos, 100,28)  
+  }
+  
+  function marcaPontos(){
+    if(yAtor < 2.8){
+      meusPontos += 1;
+    }
+    if(colisao){
+      if(meusPontos > 0 ){
+        meusPontos -= 1;
+      }
+    }
+      
+  }
