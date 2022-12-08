f72923163cd4ea2deec1d6daba4c798d4923ff30
VitorOdorico <ve743137@gmail.com>
2022-12-08 11:42:11 -0300

    implement to ator.js

--- app/src/ator.js
+++ app/src/ator.js
@@ -1,27 +1,46 @@
+
 // ator paramers
-let xAtor = 250;
+let xAtor = 243;
 let yAtor = 366;
 let larguraAtor = 30;
 let alturaAtor = 30;
 
 function mostraAtor(){
     image(imagemDoAtor, xAtor, yAtor, larguraAtor,alturaAtor);
-}
+  }
 
-function movimentaAtor(){
+  function movimentaAtor(){
     if(keyIsDown(UP_ARROW)){
-        yAtor -= 4;
+      yAtor -= 4;
     }
     else if(keyIsDown(DOWN_ARROW)){
-        yAtor += 4;
+      yAtor += 4;
     }
     else if(keyIsDown(LEFT_ARROW)){
-        xAtor -= 4;
+      xAtor -= 4;
     }
     else if(keyIsDown(RIGHT_ARROW)){
-        xAtor += 4;
+      xAtor += 4;
     }
-}
+  }
+  // reset position
+  function margemBordas (){
+    if(yAtor < 2){
+      yAtor = 366;
+      xAtor = 243;
+    }
+    if(yAtor > 400){
+      yAtor = 366;
+     
+    }
+    if(xAtor < 2){
+      xAtor = 2;
+    }
+    if(xAtor > 470){
+      xAtor = 470;
+    }
+  }
+  
 
 function Win (){
     if(yAtor < 0){
